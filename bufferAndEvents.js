f252d27eda6d347f874805e1983391db55836b63
Obed Nuertey <onuertey1997@gmail.com>
2023-08-28 02:00:40 +0000

    my first commit

--- bufferAndEvents.js
+++ bufferAndEvents.js
@@ -1,36 +1,42 @@
 const EventEmitter = require('events');
 
-class DataBuffer extends EventEmitter {
-    constructor() {
-        super();
-        this.buffer = [];
-    }
+class DataBuffer extends EventEmitter{
+	constructor(bufferSize, charset){
+		super();
+		this.bufferSize = 0;
+		this.buffer = new Buffer.alloc(bufferSize); //This is bad
+		this.charset = charset;
+		this.newData
+	}
 
-    addData(data) {
-        this.buffer.push(data);
-        this.emit('dataAdded', data);
-    }
+	addData(data){
+		this.newData = new Buffer.from(data, this.charset);
 
-    processBuffer() {
-        setInterval(() => {
-            if (this.buffer.length > 0) {
-                const data = this.buffer.shift();
-                this.emit('dataProcessed', data);
-            }
-        }, 1000);
-    }
-}
+		this.buffer = Buffer.concat([this.buffer, this.newData]);
+		this.bufferSize = this.bufferSize + this.buffer.byteLength;
+		this.emit('dataAdded', data);
+	}
 
-const buffer = new DataBuffer();
+	processBuffer(){
+		let i = this.bufferSize;
+		setInterval(()=>{
+			if(i > 0){
+				this.buffer = this.buffer.slice(0, this.buffer.byteLength - 2);
+				this.emit('dataProcessed', this.buffer);
+				i--;
+			}
+		}, 1000);
+	}
+};
 
-buffer.on('dataAdded', (data) => {
-    console.log(`Data added to buffer: ${data}`);
-});
+const buffer = new DataBuffer(8, "utf-8");
 
-buffer.on('dataProcessed', (data) => {
-    console.log(`Data processed from buffer: ${data}`);
-});
-
-buffer.addData('Hello');
-buffer.addData('World');
+buffer.on('dataAdded', (data)=>{
+	console.log(`Data added to buffer: ${data}`);
+}).on('dataProcessed', (data)=>{
+	console.log(`Data processed from buffer: ${data.toString()}`);
+}).addData("Hello this is it");
+buffer.addData(" yes this is it");
+buffer.addData(" Okay let's go");
+buffer.addData(" let's do this");
 buffer.processBuffer();
